94510b163b67c0727f332f750b3db753f7358b2d
wendy-aik-maung <wendy@nexbe.sg>
2025-05-06 01:04:52 -0400

    create carmodel and carmake

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # path(route='login', view=views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('register', views.registration, name='register'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
